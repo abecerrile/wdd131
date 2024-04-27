0f26db7be02fb322186db5401795408f3b6beaaf
abecerrile <bec23017@byui.edu>
2024-04-26 23:43:24 -0600

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var currentYear = new Date().getFullYear();
-
-    document.querySelector('footer p:first-child').textContent = "© " + currentYear + " Alberto Becerril | Mexico City";
+    document.querySelector('footer p').textContent = "\u00A9 " + currentYear + " Alberto Becerril | Mexico City";
   
     var lastModifiedDate = document.lastModified;
-    
     document.querySelector('footer p:nth-child(2)').textContent = "Last modified: " + lastModifiedDate;
 });
