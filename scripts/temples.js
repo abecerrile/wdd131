6087d16599a7006ac592fa6a5699887016b5fe4f
abecerrile <bec23017@byui.edu>
2024-05-04 16:11:09 -0600

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,10 @@ let currentYear = new Date().getFullYear();
 let lastModifiedDate = document.lastModified;
 
 document.querySelector('footer p').innerHTML = `&copy; ${currentYear} | Alberto Becerril | Mexico City <br><br> Last modified: ${lastModifiedDate}`;
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('nav');
+
+hamburgerMenu.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
